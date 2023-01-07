7c73e0b028700c3d356e7eb371953e44c78fda1a
takanoria0612 <ta.kanorioh.my.zsh@gmail.com>
2023-01-07 19:09:18 +0900

    Expressions

---
+++ hello-world/Expressions.ml
@@ -0,0 +1,6 @@
+(*The primary piece of OCaml syntax is the expression. Just like programs in 
+   imperative languages are primarily buildt out of commands, programs in 
+   functional languages are primarily built out of expressions. Examples of 
+   expreessions include 2+2 and increment 21.*)
+
+   
